4cba0a947203efaa44a849bf749034af3987baa6
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-05-24 14:26:45 -0400

    Deleted unncessary line of code.

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
-// const db = require("./models");
 const app = express();
 
 app.use(logger("dev"));
@@ -12,11 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
-// app.use(require("./public/api.js"));
-
-
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
     console.log(`Helm to 108! Server is running on: http://localhost:${PORT}/ \n`);
