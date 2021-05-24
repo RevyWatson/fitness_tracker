be1d0708104f397837b5d3a9bb5c6b9ee0fa7028
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-05-24 14:27:21 -0400

    Updated model.

---
+++ models/Workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    Workouts: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Select 'cardio' or 'resistance'!",
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Type in an exercise",
+        },
+        duration: {
+            type: Number,
+            required: "Enter a duration of time"
+        },
+        distance: {
+            type: Number,
+            required: "Enter the distance amount"
+        },
+        weight: {
+            type: Number,
+            required: "Enter amount of weight"
+        },
+        reps: {
+            type: Number,
+            required: "Enter number of reps"
+        },
+        sets: {
+            type: Number,
+            required: "Enter number of sets"
+        }
+    }]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
